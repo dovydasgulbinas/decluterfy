57747b1f25b01c434bdd107933efc787870a626b
Dovydas Gulbinas <dovydasgulbinas@gmail.com>
2016-12-13 20:01:53 +0200

    adding selected_features

--- main.py
+++ main.py
@@ -3,9 +3,26 @@ import spotipy.util as util
 
 from ml_classifier import TestClassifier
 from spotipy_client import MLearnipy
+from ml_items import DatasetFormer
 
 username = 'coder-hermes'
-default_features = ['id', 'energy']
+
+selected_features = [
+    "id",
+    "duration_ms",
+    "acousticness",
+    "tempo",
+    "speechiness",
+    "mode",
+    "danceability",
+    "liveness",
+    "instrumentalness",
+    "loudness",
+    "time_signature",
+    "energy",
+    "valence",
+    "key"
+]
 
 
 def fetch_token(username='coder-hermes'):
@@ -17,14 +34,28 @@ def main():
     if token:
         sp = MLearnipy(username, auth=token)
 
-        features = sp.get_target_and_all_other_pls(default_features)
-        print(features[0])
+        unsorted_playlist, all_playlists = sp.get_target_and_all_other_pls(selected_features)
 
-        sp.print_separator(' All other features ')
-        print(features[1])
+        # print(unsorted_playlist)
+
+        # sp.print_separator(' All other features ')
+        # print(all_playlists)
+
+        # Form a data-frame for ml analysis
+        all_pls = DatasetFormer(all_playlists, 'playlist_id')
+        unsorted_pls = DatasetFormer(unsorted_playlist, 'playlist_id')
+
+        # data magic
+        print(all_pls.target)
+        print(all_pls.feature_names)
+        print(all_pls.data)
+        print(all_pls.target_names)
+
+        print(all_pls.targets_original)
 
         # TODO: DO ML analysis with decision tree
         test_classifier_obj = TestClassifier
+
 
 if __name__ == '__main__':
     logger = logging.getLogger()
